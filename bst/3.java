1ce7db1beab56f71fa5c532d2a2b80f7ee5d24f9
Azamat <abdybaev.azamat@gmail.com>
2021-02-06 03:53:52 +0600

    .

--- bst/3.java
+++ bst/3.java
@@ -1,10 +1,17 @@
 package play;
 
-public class Node {
-    int val;
-    public Node left;
-    public Node right;
-    Node(int val) {
-        this.val = val;
+public class App 
+{
+    public static void main( String[] args )
+    {
+        BinarySearchTree bct = new BinarySearchTree();
+        bct.insert(50); 
+        bct.insert(30); 
+        bct.insert(20); 
+        bct.insert(40); 
+        bct.insert(70); 
+        bct.insert(60); 
+        bct.insert(80);
+        bct.inorder();
     }
 }
