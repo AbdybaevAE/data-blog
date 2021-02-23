af6d1d79759ad9d2e7efd02c999ce3b99bd058ff
Azamat <abdybaev.azamat@gmail.com>
2021-02-23 14:21:32 +0600

    Edit blog data

---
+++ sh/1.sh
@@ -0,0 +1,11 @@
+  go run .
+  printing hierachy for folder Folder2
+   printing hierachy for folder Folder1
+     printing file File1
+   printing file File2
+   printing file File3
+  printing hierachy for folder Folder2
+   printing hierachy for folder Folder1
+     printing file File1
+   printing file File2
+   printing file File3
